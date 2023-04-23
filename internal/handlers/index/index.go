bf3c2148de76c4e072230ae875b2c027ff4dfd96
Nick Neisen <nwneisen@gmail.com>
2023-04-22 23:31:59 -0600

    Templates almost working

--- internal/handlers/index/index.go
+++ internal/handlers/index/index.go
@@ -1,10 +1,13 @@
 package index
 
 import (
+	"bytes"
+	"fmt"
 	"nwneisen/go-proxy-yourself/pkg/config"
 	"nwneisen/go-proxy-yourself/pkg/logger"
 	"nwneisen/go-proxy-yourself/pkg/server/handlers"
 	"nwneisen/go-proxy-yourself/pkg/server/responses"
+	"text/template"
 )
 
 // Index handler
@@ -19,9 +22,33 @@ func NewIndex(config *config.Config, logger *logger.Logger) handlers.Handler {
 	}
 }
 
+// Get returns the index.html page
 func (i Index) Get() *responses.Response {
 	i.Log().Info("Index %s handler called", i.Request().Method)
-	return responses.OK("GET method of the index handler")
+
+	// page, err := ioutil.ReadFile("web/index.html")
+	// if err != nil {
+	// 	msg := fmt.Sprintf("could not read index html file: %v", err.Error())
+	// 	i.Log().Error(msg)
+	// 	return responses.InternalServerError(msg)
+	// }
+
+	tmpl, err := template.ParseFiles("web/index.html")
+	if err != nil {
+		msg := fmt.Sprintf("could not read index html file: %v", err.Error())
+		i.Log().Error(msg)
+		return responses.InternalServerError(msg)
+	}
+
+	var doc bytes.Buffer
+	// tmpl.Execute(&doc, i.Config().Routes)
+	t, err := template.New("index").ParseFiles("web/index.html")
+	err = t.Execute(&doc, i.Config().Routes)
+	page := doc.String()
+
+	routes := fmt.Sprintf("%+v", page)
+
+	return responses.OK(string(routes))
 }
 
 // // Index old handler that is no longer really used
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 
@@ -21,7 +22,7 @@ type Config struct {
 	HttpPort  string `yaml:"httpPort" json:"httpPort" default:"80"`
 	HttpsPort string `yaml:"httpsPort" json:"httpsPort" default:"443"`
 
-	Routes map[string]Route `yaml:"routes" json:"routes"`
+	Routes map[string]*Route `yaml:"routes" json:"routes"`
 }
 
 // Possible redirect routes and their information
@@ -29,23 +30,48 @@ type Route struct {
 	EgressHostname string `yaml:"egressHostname" json:"egressHostname"`
 	Port           string `yaml:"port" json:"port"`
 
-	SAML  map[string]SAMLProvider  `yaml:"saml" json:"saml"`
-	OAuth map[string]OAuthProvider `yaml:"oAuth" json:"oAuth"`
+	SAML  map[string]*SAMLProvider  `yaml:"saml,omitempty" json:"saml,omitempty"`
+	OAuth map[string]*OAuthProvider `yaml:"oAuth,omitempty" json:"oAuth,omitempty"`
+}
+
+// String returns a string representation of the Route
+func (r *Route) String() string {
+	return fmt.Sprintf("Route{egressHostname:%s, Port:%s, SAML:%v, OAuth:%v}",
+		r.EgressHostname,
+		r.Port,
+		r.SAML,
+		r.OAuth,
+	)
 }
 
 // SAMLProvider is the information needed to connect to a SAML IDP provider
 type SAMLProvider struct {
-	Name     string `yaml:"name" json:"name"`
-	URL      string `yaml:"url "json:"url"`
+	URL      string `yaml:"url" json:"url"`
 	Issuer   string `yaml:"issuer" json:"issuer"`
 	CertPath string `yaml:"certPath" json:"certPath"`
 }
 
+// String returns a string representation of the SAMLProvider
+func (sp *SAMLProvider) String() string {
+	return fmt.Sprintf("SAML{url:%s, issuer:%s, certPath:%v}",
+		sp.URL,
+		sp.Issuer,
+		sp.CertPath,
+	)
+}
+
 // OAuthProvider is the information needed to connect to an OAuth provider
 type OAuthProvider struct {
-	Name         string `yaml:"name" json:"name"`
-	ClientId     string `yaml:"ClientId" json:"ClientId"`
-	ClientSecret string `yaml:"ClientSecret" json:"ClientSecret"`
+	ClientId     string `yaml:"clientId" json:"clientId"`
+	ClientSecret string `yaml:"clientSecret" json:"clientSecret"`
+}
+
+// String returns a string representation of the OAuthProvider
+func (oap *OAuthProvider) String() string {
+	return fmt.Sprintf("OAuth{clientId:%s, clientSecret:%s}",
+		oap.ClientId,
+		oap.ClientSecret,
+	)
 }
 
 // NewConfig creates a new config structure
